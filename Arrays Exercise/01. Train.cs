c2eeeee3a90287aa2b39ab2570d7446ae7e3b578
Kremena91 <kremito9188@abv.bg>
2024-02-25 14:08:50 +0200

    Add tasks for Exercise
    Problem Solutions for Exercise: Arrays

---
+++ Arrays Exercise/01. Train.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int countPeople = int.Parse(Console.ReadLine());
+                array[i] = countPeople;
+                sum += countPeople;
+            }
+            Console.WriteLine(string.Join(" ", array));
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
