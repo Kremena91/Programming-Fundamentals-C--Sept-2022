0606a68e603cf1e8b52ec31cbdd9760819392fb7
Kremena91 <kremito9188@abv.bg>
2024-02-25 14:16:09 +0200

    Add tasks for Exercise
    Problem Solutions for Exercise: Arrays

---
+++ Arrays Exercise/02. Common Elements.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Common_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+            string[] array2 = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+            for (int i = 0; i < array2.Length; i++)
+            {
+                for (int k = 0; k < array1.Length; k++)
+                {
+                    if (array2[i] == array1[k])
+                    {
+                        Console.Write(array2[i] + " ");
+                    }
+                }
+            }
+            
+        }
+    }
+}
