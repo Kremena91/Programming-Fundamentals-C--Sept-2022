ca348039cbd93db15c1625a58c62294ce3da68ed
Kremena91 <kremito9188@abv.bg>
2024-03-02 22:13:30 +0200

    Add tasks for Exercise
    Problem Solutions for Exercise Arrays

---
+++ Arrays Exercise/09.Kamino Factory.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Kamino_Factory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] bestSequence = new int[n];
+
+            int bestCount = 1;
+            int bestStartIndex = 0;
+            int bestSequenceSum = 0;
+            int bestSequenceIndex = 0;
+
+            int sequenceCounter = 0; // брои коя подред редица сме взели
+
+            string command;
+            while ((command = Console.ReadLine()) != "Clone them!")
+            {
+                int[] currSequence = command
+                    .Split("!", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                sequenceCounter++;
+
+                int currCount = 1;
+                int bestCurrCount = 1;
+                int startCurrIndex = 0;
+                int currSequenceSum = 0;
+
+                for (int i = 0; i < currSequence.Length - 1; i++)  // обхождаме масива от първия до предпоследния елемент, за да не изслезенм от него
+                {
+                    if (currSequence[i] == currSequence[i + 1])
+                    {
+                        currCount++;
+                    }
+                    else
+                    {
+                        currCount = 1;
+                    }
+                    if (currCount > bestCurrCount)
+                    {
+                        bestCurrCount = currCount;
+                        startCurrIndex = i;
+                    }
+                    currSequenceSum += currSequence[i];
+                }
+                currSequenceSum += currSequence[currSequence.Length - 1]; // за да добавим и последния елемент, тъй като в цикъла не е включен
+
+                if (bestCurrCount > bestCount)
+                {
+                    bestCount = bestCurrCount;
+                    bestStartIndex = startCurrIndex;
+                    bestSequenceSum = currSequenceSum;
+                    bestSequenceIndex = sequenceCounter;
+                    bestSequence = currSequence.ToArray();
+                }
+                else if (bestCurrCount == bestCount) // ако са равни
+                {
+                    if (startCurrIndex < bestStartIndex) // да вземем най-вляво
+                    {
+                        bestCount = bestCurrCount;
+                        bestStartIndex = startCurrIndex;
+                        bestSequenceSum = currSequenceSum;
+                        bestSequenceIndex = sequenceCounter;
+                        bestSequence = currSequence.ToArray();
+                    }
+                    else if (startCurrIndex == bestStartIndex)
+                    {
+                        if (currSequenceSum > bestSequenceSum)
+                        {
+                            bestCount = bestCurrCount;
+                            bestStartIndex = startCurrIndex;
+                            bestSequenceSum = currSequenceSum;
+                            bestSequenceIndex = sequenceCounter;
+                            bestSequence = currSequence.ToArray();
+                        }
+                    }
+
+                }
+
+            }
+            Console.WriteLine($"Best DNA sample {bestSequenceIndex} with sum: {bestSequenceSum}.");
+            Console.WriteLine(string.Join(" ", bestSequence));
+        }
+    }
+}
---
+++ Arrays Exercise/10. LadyBugs.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+namespace _10._LadyBugs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.  инициализираме празно поле
+            int fieldSize = int.Parse(Console.ReadLine()); // размер на полето
+            int[] field = new int[fieldSize]; // това е създаденото празно поле
+            // 2. поставяме калинки на валидните индекси
+            int[] initialIndexes = Console.ReadLine() // началните индекси на всички калинки, колекцията не се променя, затова ще използваме foreach
+                .Split().
+                Select(int.Parse).
+                ToArray();
+            foreach (int index in initialIndexes) // за всеки един от индексите трябва да инициализираме на полето калинка
+            {
+                if (index >=0 && index < field.Length) // за всеки един индекс проверяваме дали е валиден (това са валидните случаи)
+                {
+                    // щом е валиден, трябва да инициализираме калинка на този индекс (на полето на този индекс слагаме 1, за да се знае, че има калинка)
+                    field[index] = 1;
+                }
+            }
+            // 3. Стартираме играта
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] arguments = command // получения стринг като команда чрез насив го разделяме, за да достъпим всеки един аргумент
+                    .Split()
+                    .ToArray();
+                // създаваме променливи за трите команди
+                int ladyBugIndex = int.Parse(arguments[0]);
+                string direction = arguments[1]; // посоката на летене
+                int flyLength = int.Parse(arguments[2]);
+
+                // 3.1. Валидираме дали калинката не е извън полето (винаги първо се валидира дали индекса е в масива и след това се проверява какво има на този индекс)
+                if (ladyBugIndex < 0 || ladyBugIndex >= field.Length) // невалидните случаи
+                {
+                    // при невалиден индекс по условие нищо не се случва
+                    continue; // за да скипнем тази команда (continue връща до while)
+                }
+
+                // 3.2. Валидираме, че има калинка на този индекс
+                if (field[ladyBugIndex] == 0)
+                {
+                    continue; // скипваме командата, нищо не се случва (continue връща до while)
+                }
+
+                // 3.3. Опитваме да преместим калинка 
+                // надясно - ladyBugIndex + flyLength
+                // наляво - ladyBugIndex - flyLength
+                // Първо трябва да махнем нашата калинка от полето, защото тя започва да лети, т.е. напуска полето и е някъде във въздуха
+                field[ladyBugIndex] = 0; // защото калинката е излетяла
+                // Второ трябва да калкулираме къде точно ще се преземи тази калинка взависимост от посоката
+                if (direction == "left")
+                {
+                    flyLength *= -1;
+                }
+                // това ще работи и за ляво и дясно
+                // при дясно напр. 0 right 1 -> 0 + 1 = 1
+                // при ляво напр. 1 left 1 -> 1 + (-1) = 0
+                int nextIndex = ladyBugIndex + flyLength;
+                // докато слеващия индекс е валиден и на него има калинка
+                while (nextIndex >= 0 && nextIndex < field.Length && field[nextIndex] == 1)
+                {
+                    nextIndex += flyLength; // следващия индекс се преизчислява
+                }
+                // Има две възможности
+                // а) nextIndex е калкулиран някъде в полето
+                // б) nextIndex не може да се калкулира в полето
+                if (nextIndex < 0 || nextIndex >= field.Length) // т.е. ако сме излезнали от полето
+                {
+                    // б) извън полето
+                    continue; // нищо не се случва
+                }
+                // а) ако сме калкулиран следващ индекс, който е валиден
+                field[nextIndex] = 1; // равно на 1, защото калинката се приземява там
+            }
+            Console.WriteLine(string.Join(" ", field));
+        }
+    }
+}
