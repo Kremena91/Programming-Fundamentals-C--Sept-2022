d748498c4eacf585841e8d21de9e585ef86317d0
Kremena91 <kremito9188@abv.bg>
2024-03-02 22:29:28 +0200

    Add tasks for Exercise
    Problem Solution for Exercise Basic Syntax, Conditional Statements and Loops - Lab

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/11. Multiplication Table 2.0.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace _10._Multiplication_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int product = 0;
+            if (number2 > 10)
+            {
+                product = number * number2;
+                Console.WriteLine($"{number} X {number2} = {product}");
+            }
+            for (int i = number2; i <= 10; i++)
+            {
+                product = number * i;
+                Console.WriteLine($"{number} X {i} = {product}");
+            }
+        }
+    }
+}
+
