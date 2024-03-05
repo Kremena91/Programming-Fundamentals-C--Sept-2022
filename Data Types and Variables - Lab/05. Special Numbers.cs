c1f6ea6265602390fa8cb9994f300cd98324d766
Kremena91 <kremito9188@abv.bg>
2024-03-05 21:09:06 +0200

    Add task for Exercise
    Problem Solution for Exercise Data Types and Variables - Lab

---
+++ Data Types and Variables - Lab/05. Special Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int sum = 0;
+                int currentNumber = i; // числото, което вземаме цифра по цифра за да сумираме и проверим сбора дали е 5, 7 или 11
+                while (currentNumber > 0)
+                {
+                    sum += currentNumber % 10;
+                    currentNumber /= 10;    
+                }
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
