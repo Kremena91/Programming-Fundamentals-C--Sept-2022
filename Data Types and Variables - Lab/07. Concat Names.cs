6006b51c22e3f51df748c82acfa880e02d89a012
Kremena91 <kremito9188@abv.bg>
2024-03-06 22:33:46 +0200

    Add task for Exercise
    Problem Solution for Exercise Data Types and Variables - Lab

---
+++ Data Types and Variables - Lab/07. Concat Names.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07._Concat_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+            Console.WriteLine($"{first}{delimiter}{second}");
+        }
+    }
+}
