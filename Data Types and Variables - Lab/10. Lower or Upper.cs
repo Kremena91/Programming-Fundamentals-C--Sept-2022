47dec5efcbc7a4b1fd1355a11799301ab0d84f80
Kremena91 <kremito9188@abv.bg>
2024-03-07 22:31:50 +0200

    Add task for Exercise
    Problem Solution for Exercise Data Types and Variables - Lab

---
+++ Data Types and Variables - Lab/10. Lower or Upper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10._Lower_or_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+            if (char.IsLower(symbol))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else if (char.IsUpper(symbol))
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
