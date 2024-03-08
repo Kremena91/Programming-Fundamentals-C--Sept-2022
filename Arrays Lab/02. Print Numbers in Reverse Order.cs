3301aec274bade59fb04446ee7cc68ad5c128076
Kremena91 <kremito9188@abv.bg>
2024-03-08 21:48:21 +0200

    Add task for Exercise
    Problem Solution for Arrays - Lab

---
+++ Arrays Lab/02. Print Numbers in Reverse Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Print_Numbers_in_Reverse_Order
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] number = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                number[i] = currentNumber;
+            }
+            for (int i = number.Length-1; i >= 0; i--)
+            {
+                Console.Write($"{number[i]} "); 
+            }
+        }
+    }
+}
