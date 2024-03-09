90f81aa0dd32d611d4dbee65650b9d939f267762
Kremena91 <kremito9188@abv.bg>
2024-03-09 21:12:00 +0200

    Add task for Exercise
    Problem Solution for Arrays - Lab

---
+++ Arrays Lab/08. Condense Array to Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split().
+                Select(int.Parse).
+                ToArray();
+            
+            while (numbers.Length > 1) // дължината на масива
+            {
+                int[] condensed = new int[numbers.Length-1];
+
+                for (int i = 0; i < numbers.Length-1; i++) // до елемента на последната позиция
+                {
+                    condensed[i] = numbers[i] + numbers[i + 1];
+                }
+                numbers = condensed;
+            }
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
+
+
