6a6adbb40b348f6c88250be76d4a10eba3f55f2a
Kremena91 <kremito9188@abv.bg>
2024-03-10 20:59:25 +0200

    Add task for Exercise
    Problem Solution for Methods- Lab

---
+++ Methods - Lab/03. Calculations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (type == "add")
+            {
+                getAdd(num1, num2);
+            }
+            else if (type == "multiply")
+            {
+                getMultiply(num1, num2);
+            }
+            else if (type == "substract")
+            {
+                getSubstract(num1, num2);
+            }
+            else if (type == "divide")
+            {
+                getDivide(num1, num2);
+            }
+        }
+
+        static void getAdd(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+
+        static void getMultiply(int num1, int num2)
+        {
+            Console.WriteLine(num1 * num2);
+        }
+
+        static void getSubstract(int num1, int num2)
+        {
+            Console.WriteLine(num1 - num2);
+        }
+
+        static void getDivide(int num1, int num2)
+        {
+            Console.WriteLine(num1 / num2);
+        }
+    }
+}
