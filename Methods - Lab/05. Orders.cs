00ed4850dc066e6bc385e403c48399699590d281
Kremena91 <kremito9188@abv.bg>
2024-03-11 22:02:33 +0200

    Add task for Exercise
    Problem Solution for Methods- Lab

---
+++ Methods - Lab/05. Orders.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double priceCoffee = 1.50;
+            double priceWater = 1.00;
+            double priceCoke = 1.40;
+            double priceSnakcs = 2.00;
+
+            if (product == "coffee")
+            {
+                getTotalPriceOfCoffee(priceCoffee, quantity);
+            }
+            else if (product == "water")
+            {
+                getTotalPriceOfWater(priceWater, quantity);
+            }
+            else if (product == "coke")
+            {
+                getTotalPriceOfCoke(priceCoke, quantity);
+            }
+            else if (product == "snacks")
+            {
+                getTotalPriceOfSnacks(priceSnakcs, quantity);
+            }
+        }
+        static void getTotalPriceOfCoffee(double priceCoffee, int quantity)
+        {
+            Console.WriteLine($"{(priceCoffee * quantity):f2}");
+        }
+        static void getTotalPriceOfWater(double priceWater, int quantity)
+        {
+            Console.WriteLine($"{(priceWater * quantity):f2}");
+        }
+
+        static void getTotalPriceOfCoke(double priceCoke, int quantity)
+        {
+            Console.WriteLine($"{(priceCoke * quantity):f2}");
+        }
+
+        static void getTotalPriceOfSnacks(double priceSnacks, int quantity)
+        {
+            Console.WriteLine($"{(priceSnacks * quantity):f2}");
+        }
+    }
+}
