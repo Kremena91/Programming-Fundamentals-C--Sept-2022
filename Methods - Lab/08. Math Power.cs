8ba5012d103dcfbd95df865c90669c78b97d4809
Kremena91 <kremito9188@abv.bg>
2024-03-12 22:38:46 +0200

    Add task for Exercise
    Problem Solution for Methods- Lab

---
+++ Methods - Lab/08. Math Power.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+            double result = MathPower(num, power);
+            Console.WriteLine(result);
+        }
+
+        static double MathPower(double num, int power)
+        {
+            double result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= num;
+            }
+            return result;
+        }
+    }
+}
