802da8fde4f3f644ee7269cce804216601fbad99
Kremena91 <kremito9188@abv.bg>
2024-03-14 22:02:41 +0200

    Add task for Exercise
    Problem Solution for Methods- Lab

---
+++ Methods - Lab/11. Math operations.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            int secondNum = int.Parse(Console.ReadLine());
+
+            if (operation == "+")
+            {
+                int result = getAdd(firstNum, secondNum);
+                Console.WriteLine(result);
+            }
+            else if (operation == "*")
+            {
+                int result = getMultiply(firstNum, secondNum);
+                Console.WriteLine(result);
+            }
+            else if (operation == "-")
+            {
+                int result = getSubstract(firstNum, secondNum);
+                Console.WriteLine(result);
+            }
+            else if (operation == "/")
+            {
+                int result = getDivide(firstNum, secondNum);
+                Console.WriteLine(result);
+            }
+        }
+        static int getAdd(int firstNum, int secondNum)
+        {
+            int result = firstNum + secondNum;
+            return result;
+        }
+
+        static int getMultiply(int firstNum, int secondNum)
+        {
+            int result = firstNum * secondNum;
+            return result;
+        }
+
+        static int getSubstract(int firstNum, int secondNum)
+        {
+            int result = firstNum - secondNum;
+            return result;
+        }
+
+        static int getDivide(int firstNum, int secondNum)
+        {
+            int result = firstNum / secondNum;
+            return result;
+        }
+    }
+}
