125f8c03f9e5afac91d0847b01f463a708cbda01
Kremena91 <kremito9188@abv.bg>
2024-03-14 22:05:42 +0200

    Add task for Exercise
    Problem Solution for Methods- Exercise

---
+++ Methods - Exercise/01. Smallest of Three Numbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+                    
+            int minNum = GetMinNum(num1, num2, num3);
+            Console.WriteLine(minNum);
+        }
+
+        static int GetMinNum(int num1, int num2, int num3)
+        {
+            int minNum = int.MaxValue;
+
+            if (num1 <= num2 && num1 <= num3)
+            {
+                minNum = num1;
+            }
+            else if (num2 <= num1 && num2 <= num3)
+            {
+                minNum = num2;
+            }
+            else if (num3 <= num1 && num3 <= num2)
+            {
+                minNum = num3;
+            }
+            return minNum;
+        }
+    }
+}
+
