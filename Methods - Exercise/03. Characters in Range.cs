94925eaa121fac577d63c92cf9c07ccd26015b5e
Kremena91 <kremito9188@abv.bg>
2024-03-18 21:46:51 +0200

    Add task for Exercise
    Problem Solution for Methods- Exercise

---
+++ Methods - Exercise/03. Characters in Range.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+
+            int startIndex = (int)start;
+            int endIndex = (int)end;
+
+            PrintTheCharsInRange(start, end);
+            
+        }
+        static void PrintTheCharsInRange(int startIndex, int endIndex)
+        {
+            char currCh;
+            if (startIndex > endIndex)
+            {
+                for (int i = endIndex + 1; i < startIndex; i++)
+                {
+                    currCh = (char)i;
+                    Console.Write($"{currCh} ");
+                }
+            }
+            for (int i = startIndex + 1; i < endIndex; i++)
+            {
+                currCh = (char)i;
+                Console.Write($"{currCh} ");
+            }
+        }
+    }
+}
