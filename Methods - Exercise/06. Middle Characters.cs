5bd753c6ff16b763fa8139a3483e78574fddd8be
Kremena91 <kremito9188@abv.bg>
2024-03-19 22:39:39 +0200

    Add task for Exercise
    Problem Solution for Methods- Exercise

---
+++ Methods - Exercise/06. Middle Characters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol = Console.ReadLine();
+            int length = symbol.Length;
+
+            string result = GetMiddleCharacter(symbol);
+            Console.WriteLine(result);
+        }
+
+        static string GetMiddleCharacter(string symbol)
+        {
+            string result= " ";
+            
+            if (symbol.Length % 2 == 0)
+            {
+                result = symbol[symbol.Length / 2 - 1].ToString() + symbol[symbol.Length / 2].ToString();
+            }
+            else if (symbol.Length % 2 != 0)
+            {
+                result = symbol[symbol.Length / 2].ToString();
+            }
+            return result;
+        }
+        
+            
+    }
+}
