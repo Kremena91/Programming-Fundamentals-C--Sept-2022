0573fb8b89c0d447d76228d267b4defcb46974b5
Kremena91 <kremito9188@abv.bg>
2024-03-19 22:38:51 +0200

    Add task for Exercise
    Problem Solution for Methods- Exercise

---
+++ Methods - Exercise/05. Add and Subtract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            int sum = GetSumNum1AddToNum2(num1, num2);
+            int substract = GetSumSubstractNum3(sum, num3);
+            Console.WriteLine(substract);
+            
+        }
+
+        static int GetSumNum1AddToNum2(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+        static int GetSumSubstractNum3(int sum, int num3)
+        {
+            int substract = sum - num3;
+            return substract;
+        }
+    }
+}
