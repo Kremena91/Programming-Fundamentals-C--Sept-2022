e4be8a08d0d1fe046949c296ff3ccd511a651207
Kremena91 <kremito9188@abv.bg>
2024-03-20 22:46:00 +0200

    Add task for Exercise
    Problem Solution for Methods Exercise

---
+++ Methods - Exercise/10. Top Number.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _10._Top_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                if (devisibleByEigth(i) && oddDigit(i))
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+            static bool devisibleByEigth(int num)
+            {
+
+                int sum = 0;
+                while (num > 0)
+                {
+                    sum += num % 10;
+                    num /= 10;
+                }
+                if (sum % 8 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            static bool oddDigit(int num)
+            {
+
+                int count = 0;
+                while (num > 0)
+                {
+                    if ((num % 10) % 2 == 1)
+                    {
+                        count++;
+                    }
+                    num /= 10;
+                }
+                if (count >= 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+    }
