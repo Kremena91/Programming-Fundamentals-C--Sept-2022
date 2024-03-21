ba0564866302f355dab6fe0084b455525bd74ce5
Kremena91 <kremito9188@abv.bg>
2024-03-21 21:35:50 +0200

    Add task for Exercise
    Problem Solution for Lists Lab

---
+++ Lists - Lab/01. Sum Adjacent Equal Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+
+            for (int i = 0; i < numbers.Count-1; i++) // не трябва да стигаме до последното число
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] = numbers[i] + numbers[i+1];
+                    numbers.RemoveAt(i + 1); // къде точно ще премахнем елемент, след като сумираме, ел. на позиция i + 1 става излишно
+                    i = -1; // за да се върнем отначало на for цикъла,така проверява пак отначало за еднакви числа и сумира
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
