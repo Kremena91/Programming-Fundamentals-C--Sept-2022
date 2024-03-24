4f6b2f9eabcdee97c68ef8b6bc179be8735cb86d
Kremena91 <kremito9188@abv.bg>
2024-03-24 23:05:13 +0200

    Add task for Exercise
    Problem Solution for Lists Lab

---
+++ Lists - Lab/04. List of Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List <string> products = new List<string>(); // създаваме лист за продуктите
+
+            for (int i = 0; i < n; i++)
+            {
+                string product = Console.ReadLine();
+                products.Add(product);  // добавяме в листа всеки продукт
+            }
+            products.Sort(); // подреждаме по възходящ ред
+
+            for (int i = 0; i < products.Count; i++) 
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
