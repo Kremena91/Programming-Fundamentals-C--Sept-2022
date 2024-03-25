29fa3b16e9d0fe9adb603e5165c8669bb26d9c0f
Kremena91 <kremito9188@abv.bg>
2024-03-25 22:55:38 +0200

    Add task for Exercise
    Problem Solution for Lists Exercise

---
+++ Lists - Exercise/01. Train.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = command
+                    .Split();
+
+                if (cmdArgs[0] == "Add")
+                {
+                    int numberToAdd = int.Parse(cmdArgs[1]);
+                    wagons.Add(numberToAdd);
+                }
+                else
+                {
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        int passengers = int.Parse(cmdArgs[0]);
+                        if ((wagons[i] + passengers) <= maxCapacity)
+                        {
+                        wagons[i] += passengers;
+                        break;
+                    }  
+                }
+            }
+                
+         }
+            Console.WriteLine(String.Join(" ", wagons));
+        }
+    }
+}
