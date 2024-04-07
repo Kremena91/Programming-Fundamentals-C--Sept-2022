689e13c27b8f90ebc018114316d57e6725562a87
Kremena91 <kremito9188@abv.bg>
2024-04-07 21:26:38 +0300

    Add task for Exercise
    Problem Solution for Lists Exercise

---
+++ Lists - Exercise/07. Append Arrays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine()
+                 .Split("|")
+                 .ToList();
+
+            items.Reverse();
+
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                List<string> numbers = items[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    result.Add(numbers[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
