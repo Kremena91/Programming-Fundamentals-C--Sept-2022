8a523a2733c26aaee1fe3f032cc99e6a46f274e9
Kremena91 <kremito9188@abv.bg>
2024-06-28 22:48:43 +0300

    Add task for Exercise
    Problem Solution for Objects and Classes - Exercise

---
+++ Objects and Classes - Exercise/03. Articles 2.0.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Articles_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countArticles = int.Parse(Console.ReadLine());
+
+            List<Article> articles = new List<Article>();
+
+            string[] input;
+
+            for (int i = 0; i < countArticles; i++)
+            {
+                input = Console.ReadLine().Split(", ");
+
+                Article article = new Article(input[0], input[1], input[2]);
+
+                articles.Add(article);
+
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, articles));
+        }
+    }
+
+    class Article
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
+
