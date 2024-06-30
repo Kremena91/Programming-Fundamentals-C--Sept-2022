a667d5052c34227843686012642cf0e0b8effe44
Kremena91 <kremito9188@abv.bg>
2024-06-30 16:20:55 +0300

    Add task for Exercise
    Problem Solution for Associative Arrays - Lab

---
+++ Associative Arrays - Lab/03. Word Synonyms.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ["string", new List<string> {"str1", "str2"}], т.е. към една дума може да имаме няколко синонима
+            Dictionary<string, List<string>> synonyms
+                = new Dictionary<string, List<string>>();
+
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!synonyms.ContainsKey(word)) // Ако думата не се сърдържа в нашия речник 
+                {
+                    List<string> newListForCurrentWord = new List<string>(); // създаваме нов лист за тази дума
+                    synonyms.Add(word, newListForCurrentWord); // искаме да добавиш тази дума 
+                }
+                synonyms[word].Add(synonym); // искаме в нашия речник на дадената дума да добавим синонима, който сме открили 
+            }
+
+            foreach (var item in synonyms)
+            {
+                Console.WriteLine($"{item.Key} - {string.Join(", ", item.Value)}");
+            }
+        }
+    }
+}
+
