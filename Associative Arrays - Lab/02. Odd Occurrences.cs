f213fd3ae09d4b7be3a2b177bbed7c97fb1d41a9
Kremena91 <kremito9188@abv.bg>
2024-06-30 16:20:06 +0300

    Add task for Exercise
    Problem Solution for Associative Arrays - Lab

---
+++ Associative Arrays - Lab/02. Odd Occurrences.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Иска се от нас като изход да извадим всяка част от входящия стринг (без спейсовете), която се среща нечетен брой пъти в него. В дадения пример C# и PHP се срещат по 3 пъти, а 1 и 5 - по въднъж. Java и 3 се срещат по 2 пъти и затова не са изведени на изхода
+            
+            string[] words = Console.ReadLine().ToLower().Split();
+
+            // key - думата
+            // value - колко пъти се среща думата
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordInLowerCase = word.ToLower(); // прави думата с малки букви
+
+                if(counts.ContainsKey(wordInLowerCase)) // Ако думата се съдържа в речника, броим колко пъти се среща
+                {
+                    counts[wordInLowerCase]++;
+                }
+                else // в противен случай я добавяме
+                {
+                    counts.Add(wordInLowerCase, 1);
+                } 
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 == 1) // ако думата се среща нечетен брой пъти
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
