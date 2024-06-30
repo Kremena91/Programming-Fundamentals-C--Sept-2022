e40abb74b53e4b4b5bdf96ce7fb1773c24b92db5
Kremena91 <kremito9188@abv.bg>
2024-06-30 16:19:15 +0300

    Add task for Exercise
    Problem Solution for Associative Arrays - Lab

---
+++ Associative Arrays - Lab/01. Count Real Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // key - стойността 
+            // value - колко пъти се среща числото
+            SortedDictionary<double, int> numberOcc
+                = new SortedDictionary<double, int>();
+
+            double[] inputNumbers = Console.ReadLine() // четем входните данни като масив
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            foreach (double currentNumber in inputNumbers)
+            {
+                if (!numberOcc.ContainsKey(currentNumber)) // Ако числото не съществува го добави
+                {
+                    numberOcc.Add(currentNumber, 0); // първоначално числото се среща 0 пъти
+                }
+                numberOcc[currentNumber] += 1; // увеличаваме с 1, когато срещнем същото число
+            }
+
+            foreach (var item in numberOcc)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
