17ca5b58b4ccfc27872b4b6d893c880a175193ac
Kremena91 <kremito9188@abv.bg>
2024-07-02 21:45:51 +0300

    Add task for Exercise
    Problem Solution for Associative Arrays - Exercise

---
+++ Associative Arrays - Exercise/01. Count Chars in a String.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // key - символ ch
+            // value - колко пъти се среща
+            Dictionary<char, int> occurances =
+                new Dictionary<char, int>();
+
+            string text = Console.ReadLine();
+            foreach (char ch in text)
+            {
+                if (ch != ' ') // ako ch e различно от празно пространство
+                {
+                    // провери дали в речника се съдържа ключ, който отговаря на този ch
+                    if (!occurances.ContainsKey(ch)) // ако не съществува
+                    {
+                        occurances[ch] = 0;
+                    }
+
+                    occurances[ch]++;
+                }
+            }
+
+            foreach (var kvp in occurances)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
