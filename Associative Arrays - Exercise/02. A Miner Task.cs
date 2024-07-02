fadd30972cc4914fa6a881416c58afbb2ead0b29
Kremena91 <kremito9188@abv.bg>
2024-07-02 21:46:56 +0300

    Add task for Exercise
    Problem Solution for Associative Arrays - Exercise

---
+++ Associative Arrays - Exercise/02. A Miner Task.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // key - името на ресурса
+            // value - количеството
+            Dictionary<string, int> resources =
+                new Dictionary<string, int>();
+
+            string resource;
+            while ((resource = Console.ReadLine()) != "stop") // на четните редове получаваме ресурс
+            {
+                int quantity = int.Parse(Console.ReadLine()); // на нечетните редове количество
+
+                if (!resources.ContainsKey(resource)) //ако не съществува ключ за този ресурс
+                {
+                    resources[resource] = 0; // като първоначалната стойност задаваме 0, т.е. първоначално кличеството на този ресурс е 0
+                }
+                resources[resource] += quantity; // всеки път независимо дали съществува или не, към ресурсите на този ресурс ще прибавяме количеството
+
+                // Втори начин
+                //if (!resource.Contains(resource)) // Ако не съществува този ресурс
+                //{
+                //    resources[resource] = quantity; // в ресурсите срещу негово име добавяме количеството
+                //}
+                //else
+                //{
+                //    resources[resource] += quantity; // ако вече съществува срещу неговото име прибавяме количеството
+                //}
+            }
+
+            foreach (var rqp in resources) // за всяка двойка в ресурсите принтираме ресурс и количество
+            {
+                Console.WriteLine($"{rqp.Key} -> {rqp.Value}");
+            }
+        }
+    }
+}
