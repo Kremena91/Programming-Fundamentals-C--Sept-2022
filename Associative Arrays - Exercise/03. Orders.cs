f16ec96579d488bf3bff2813e3db6e2f0180be0a
Kremena91 <kremito9188@abv.bg>
2024-07-03 20:19:03 +0300

    Add task for Exercise
    Problem Solution for Associative Arrays - Exercise

---
+++ Associative Arrays - Exercise/03. Orders.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> product = new Dictionary<string, List<double>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "buy")
+            {
+                string[] cmdArgs = input.Split();
+                string name = cmdArgs[0];
+                double price = double.Parse(cmdArgs[1]);
+                double quantity = double.Parse(cmdArgs[2]);
+
+                if (!product.ContainsKey(name))
+                {
+                    List<double> addToList = new List<double>() {price, quantity};
+                    product.Add(name, addToList);
+                }
+                else
+                {
+                    List<double> currentList = product[name];
+
+                    currentList[0] = price;
+                    currentList[1] += quantity;
+                    product[name] = currentList;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in product)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value[0] * item.Value[1]:f2}");
+            }
+
+        }
+    }
+}
