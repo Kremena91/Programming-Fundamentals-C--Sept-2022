e527ee5b50c577f284900d92087145b922637083
Kremena91 <kremito9188@abv.bg>
2024-07-04 21:34:04 +0300

    Add task for Exercise
    Problem Solution for Associative Arrays - Exercise

---
+++ Associative Arrays - Exercise/07. Company Users.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companyList = new Dictionary<string, List<string>>();
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] splitComand = command.Split("->").ToArray();
+                string companyName = splitComand[0];
+                string employeeID = splitComand[1];
+
+                if (!companyList.ContainsKey(companyName))
+                {
+                    companyList.Add(companyName, new List<string>());
+                }
+                if (!companyList[companyName].Contains(employeeID))
+                {
+                    companyList[companyName].Add(employeeID);
+                }
+            }
+            foreach (var kvp in companyList)
+            {
+                Console.WriteLine($"{kvp.Key}");
+
+                foreach (var emploeeID in kvp.Value)
+                {
+                    Console.WriteLine($"--{emploeeID}");
+                }
+            }
+        }
+    }
+}
