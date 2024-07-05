f6e254fc01945a44efc8669f6c477323eb0b58e9
Kremena91 <kremito9188@abv.bg>
2024-07-05 21:52:26 +0300

    Add task for Exercise
    Problem Solution for Text Processing - Lab

---
+++ Text Processing - Lab/04. Text Filter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Text_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                .Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (string currentBannedWord in bannedWords)
+            {
+                // Console.WriteLine(new string('*', 10)); - задаваме символа и колко на брой пъти искаме да се принтира този символ
+                text = text.Replace(currentBannedWord, new string('*', currentBannedWord.Length));
+                // презаписваме в нов текст и задаваме думата за премахване, нов стринг със символа и дължината на думата за премахване
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
