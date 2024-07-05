c30c8024e716b5f6ba10923fd8bf0b03ebffbe45
Kremena91 <kremito9188@abv.bg>
2024-07-05 21:51:38 +0300

    Add task for Exercise
    Problem Solution for Text Processing - Lab

---
+++ Text Processing - Lab/03. Substring.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string specialWord = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(specialWord)) // докато нашия текст съдържа специалната дума, която трябва да премахваме от текста
+            {
+                int startIndex = text.IndexOf(specialWord); // искаме от целия текст да намерим на кой индекс стартира специална дума
+                text = text.Remove(startIndex, specialWord.Length);
+                // премахни думата като използваш метода removе ( от кой индекс стартира думата и колко е дълга думата)
+                // след това презапиши text наново и се върни в while цикъла да търсиш дали след като си изтрил тази дума все още се съдържа някоя, която отговаря на спец. думи
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
