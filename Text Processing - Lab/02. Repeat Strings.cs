cd5b3b20aad675b56b1fbd021da2acfd8d19a07b
Kremena91 <kremito9188@abv.bg>
2024-07-05 21:50:27 +0300

    Add task for Exercise
    Problem Solution for Text Processing - Lab

---
+++ Text Processing - Lab/02. Repeat Strings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split();
+
+            string result = string.Empty; // което е същото като " "
+
+            foreach (string currentWord in words)
+            {
+                for (int i = 0; i < currentWord.Length; i++) // трябва да принтираме думата колкото пъти е нейната дължина
+                {
+                    result += currentWord;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
