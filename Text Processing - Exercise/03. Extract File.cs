ac2209e3ce7e4f8af287b9a4b075371bc1a221a3
Kremena91 <kremito9188@abv.bg>
2024-07-09 22:43:16 +0300

    Add task for Exercise
    Problem Solution for Text Processing - Exercise

---
+++ Text Processing - Exercise/03. Extract File.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03._Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split("\\"); // => [C:, Internal, training-internal, Template.pptx]
+            string file = input[input.Length - 1]; // вземаме последния индекс => Template.pptx
+            string[] splitLastWord = file.Split('.'); // => създаваме нов масив и го сплитваме [Template, pptx]
+            string fileName = splitLastWord[0];
+            string extension = splitLastWord[1];
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
