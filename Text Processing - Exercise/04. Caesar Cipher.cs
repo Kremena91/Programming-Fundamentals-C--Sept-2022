a3d297238c6bf2bf6220d7052ffdb161bf549f49
Kremena91 <kremito9188@abv.bg>
2024-07-11 22:12:26 +0300

    Add task for Exercise
    Problem Solution for Text Processing - Exercise

---
+++ Text Processing - Exercise/04. Caesar Cipher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine(); // Programming is cool!
+            StringBuilder sb = new StringBuilder(); // модифицира стринга
+            foreach (char currCahr in input)
+            {
+                int currPositon = currCahr; // currChar = "P" in int => 80
+                currPositon += 3;
+                sb.Append((char)currPositon); // 83 int into char => 'S'
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
