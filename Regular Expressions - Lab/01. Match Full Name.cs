05ed7485d89d189bc08f1b842453ff08ff6e7692
Kremena91 <kremito9188@abv.bg>
2024-07-17 21:50:51 +0300

    Add task for Exercise
    Problem Solution for Regular Expressions - Lab

---
+++ Regular Expressions - Lab/01. Match Full Name.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b"; // \b (boundaries - граница) - това което търсим трябва преди него да има празно място (не трябва да има някаква дръга дума и др.); казваме му че искаме думата да започва по този начин 
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollection = regex.Matches(input);
+
+            foreach (Match item in matchCollection)
+            {
+                Console.Write(item.Value + " "); // принтира само успешните мачвания
+            }
+        }
+    }
+}
